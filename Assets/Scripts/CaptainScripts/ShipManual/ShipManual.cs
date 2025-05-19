1f2852b18eed9bdf1bcd2a90c5a0691c071fe95a
jschott22 <apeman10@outlook.com>
2025-05-18 21:11:09 -0700

    Ship Manual Progress

--- Assets/Scripts/CaptainScripts/ShipManual/ShipManual.cs
+++ Assets/Scripts/CaptainScripts/ShipManual/ShipManual.cs
@@ -1,16 +1,86 @@
+/*
+    ShipManual.cs
+    - Handles inputs for communicator keyboard
+    - Displays to code screen
+    Contributor(s): Jake Schott
+    Last Updated: 5/17/2025
+*/
+
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine.UI;
 
 public class ShipManual : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //CLASS CONSTANTS
+    private static string[] WELCOME_MESSAGES = new string[]
     {
-        
+        "GOOD MORNING, CAPTAIN",
+        "WELCOME BACK, CAPTAIN",
+        "NICE TO SEE YOU, CAPTAIN"
+    };
+    private static float INTRO_TIME = 2.0f;
+
+    public GameObject welcome_screen;
+    public GameObject home_screen;
+    public GameObject curr_screen;
+
+    private bool currently_enabled = false;
+    private Coroutine power_on_coroutine = null;
+
+    public int pickWelcomeMessage()
+    {
+        return (Random.Range(0, WELCOME_MESSAGES.Length));
+    }
+    public bool getCurrentlyEnabled()
+    {
+        return currently_enabled;
+    }
+    public bool getCurrentlyAnimating()
+    {
+        return (power_on_coroutine != null);
+    }
+    
+    IEnumerator powerOn(int msg)
+    {
+        welcome_screen.transform.GetChild(0).GetComponent<TMP_Text>().SetText("");
+        welcome_screen.SetActive(true);
+
+        for (int i = 0; i < WELCOME_MESSAGES[msg].Length; i++)
+        {
+            welcome_screen.transform.GetChild(0).GetComponent<TMP_Text>().SetText(welcome_screen.transform.GetChild(0).GetComponent<TMP_Text>().text + WELCOME_MESSAGES[msg][i]);
+            yield return new WaitForSeconds((INTRO_TIME * 0.5f) / WELCOME_MESSAGES[msg].Length);
+        }
+
+        yield return new WaitForSeconds(INTRO_TIME * 0.5f);
+
+        welcome_screen.SetActive(false);
+        home_screen.SetActive(true);
+        currently_enabled = true;
+        transform.GetComponent<ShipManualOnOff>().reactivate();
+
+        power_on_coroutine = null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void powerSwitch(bool to_switch_to, int msg)
     {
-        
+        if (to_switch_to == true)
+        {
+            if (power_on_coroutine != null)
+            {
+                StopCoroutine(power_on_coroutine);
+            }
+            power_on_coroutine = StartCoroutine(powerOn(msg));
+        }
+        else
+        {
+            currently_enabled = false;
+            home_screen.SetActive(false);
+            welcome_screen.SetActive(false);
+            transform.GetComponent<ShipManualOnOff>().reactivate();
+        }
     }
 }
--- Assets/Scripts/CaptainScripts/ShipManual/ShipManualOnOff.cs
+++ Assets/Scripts/CaptainScripts/ShipManual/ShipManualOnOff.cs
@@ -9,105 +9,98 @@ using Unity.Netcode;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
+
 
 public class ShipManualOnOff : NetworkBehaviour, IControllable
 {
     //CLASS CONSTANTS
-    private static float PUSH_TIME = 0.5f;
+    private static float SWITCH_TIME = 0.5f;
 
     private string CONTROL_NAME = "SHIP MANUAL";
     private List<string> CONTROL_DESCS = new List<string> { "TURN ON", "TURN OFF" };
     private List<int> CONTROL_INDEXES = new List<int>() { 6 };
     private List<Button> BUTTONS = new List<Button>();
 
-    public List<GameObject> buttons; // 0-3 is up, 4-7 is down
+    public GameObject power_switch;
 
-    private Vector3[] initial_pos = new Vector3[2];
-    private Vector3 push_direction = new Vector3(0.0022f, -0.0052f, 0f);
-
-    private Coroutine sequence_change_coroutine = null;
+    private Coroutine power_change_coroutine = null;
 
     private static HUDInfo hud_info = null;
     private void Start()
     {
         hud_info = new HUDInfo(CONTROL_NAME);
-        BUTTONS.Add(new Button(CONTROL_DESCS[0], CONTROL_INDEXES[0], false, true));
+        BUTTONS.Add(new Button(CONTROL_DESCS[0], CONTROL_INDEXES[0], true, true));
         hud_info.setButtons(BUTTONS);
-
-        //set initial positions
-        /*for (int i = 0; i < buttons.Count; i++)
-        {
-            initial_pos[i] = buttons[i].transform.localPosition;
-        }*/
     }
     public HUDInfo getHUDinfo(GameObject current_target)
     {
         return hud_info;
     }
-
-    IEnumerator sequenceAdjustment(int index)
+    public void reactivate()
     {
-        //set buttons to initial positions
-        for (int i = 0; i < buttons.Count; i++)
+        bool ce = transform.GetComponent<ShipManual>().getCurrentlyEnabled();
+        if (ce == true)
         {
-            buttons[i].transform.localPosition = initial_pos[i];
+            BUTTONS[0].updateDesc(CONTROL_DESCS[1]);
         }
-
-        Vector3 final_pos = initial_pos[index] + push_direction;
-
-        for (int i = 0; i <= 1; i++)
+        else
         {
-            float half_time = PUSH_TIME * 0.5f;
-            float push_time = half_time;
-
-            while (push_time > 0.0f)
-            {
-                float dt = Mathf.Min(Time.deltaTime, 1.0f / 30.0f);
-                push_time = Mathf.Max(0.0f, push_time - dt);
-
-                float push_percentage = 1.0f - (push_time / half_time);
-                if (i == 1)
-                {
-                    push_percentage = (push_time / half_time);
-                }
-
-                buttons[index].transform.localPosition =
-                    new Vector3(Mathf.Lerp(initial_pos[index].x, final_pos.x, push_percentage),
-                                Mathf.Lerp(initial_pos[index].y, final_pos.y, push_percentage),
-                                Mathf.Lerp(initial_pos[index].z, final_pos.z, push_percentage));
-
-                yield return null;
-            }
+            BUTTONS[0].updateDesc(CONTROL_DESCS[0]);
         }
 
         BUTTONS[0].updateInteractable(true);
+    }
 
-        sequence_change_coroutine = null;
+    IEnumerator powerChangeAdjustment(bool to_switch_to, int msg)
+    {
+        float switch_time = SWITCH_TIME;
+
+        //flip switch, fill meter
+        while (switch_time > 0)
+        {
+            float dt = Mathf.Min(Time.deltaTime, 1.0f / 30.0f);
+            switch_time = Mathf.Max(0.0f, switch_time - dt);
+
+            float lever_angle = Mathf.Lerp(-50f, -100f, switch_time / SWITCH_TIME);
+
+            if (to_switch_to == true)
+            {
+                lever_angle = Mathf.Lerp(-50f, -100f, 1.0f - (switch_time / SWITCH_TIME));
+            }
+
+            power_switch.transform.localRotation =
+                Quaternion.Euler(lever_angle, -90f, 90f);
+
+            yield return null;
+        }
+
+        transform.GetComponent<ShipManual>().powerSwitch(to_switch_to, msg);
+
+        power_change_coroutine = null;
     }
 
     public void handleInputs(List<KeyCode> inputs, GameObject current_target, float dt, int position)
     {
-        if (sequence_change_coroutine == null)
+        if (power_change_coroutine == null)
         {
-            for (int i = 0; i <= 1; i++)
+            if (ControlScript.checkInputIndex(CONTROL_INDEXES[0], inputs) && transform.GetComponent<ShipManual>().getCurrentlyAnimating() == false)
             {
-                if (ControlScript.checkInputIndex(CONTROL_INDEXES[i], inputs))
-                {
-                    BUTTONS[i].toggle(0.1f);
-                    transmitDestructSequenceChangeRPC(i);
-                    return;
-                }
+                BUTTONS[0].toggle(0.2f);
+                int welcome_message = transform.GetComponent<ShipManual>().pickWelcomeMessage();
+                bool currently_enabled = transform.GetComponent<ShipManual>().getCurrentlyEnabled();
+                transmitShipManualPowerChangeRPC(!currently_enabled, welcome_message);
             }
         }
     }
 
     [Rpc(SendTo.Everyone)]
-    private void transmitDestructSequenceChangeRPC(int button_index)
+    private void transmitShipManualPowerChangeRPC(bool to_switch_to, int msg)
     {
-        if (sequence_change_coroutine != null)
+        if (power_change_coroutine != null)
         {
-            StopCoroutine(sequence_change_coroutine);
+            StopCoroutine(power_change_coroutine);
         }
-        sequence_change_coroutine = StartCoroutine(sequenceAdjustment(button_index));
+        power_change_coroutine = StartCoroutine(powerChangeAdjustment(to_switch_to, msg));
     }
 }
